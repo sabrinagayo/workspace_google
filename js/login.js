cebd75f6c4cfc9fa1b61b8209c4ddbd29e2e5fe9
sabrinagayo <sabrinagayo6@gmail.com>
2020-08-25 21:49:29 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,6 +5,10 @@ function onSignIn(googleUser) {
 	googleUser.preventDefault();
 	var profile = googleUser.getBasicProfile();
 	var nombreUsuario = profile.getName();
+	sessionStorage.setItem('logueado', 'true');//cambia el estado del usuario a logueado
+	localStorage.setItem('nombreUsuario', nombreUsuario);//guarda en un local storage el nombre del usuario
+	window.location.replace("index.html");//redirije al index.html
+	return true;//hace que al final la información se envíe al servidor
 	/*
 	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
 	console.log('Name: ' + profile.getName());
@@ -13,12 +17,12 @@ function onSignIn(googleUser) {
 	var id_token = googleUser.getAuthResponse().id_token;
 	console.log(id_token);
 	*/
-		if (nombreUsuario != null || nombreUsuario !=0) {
+		/*if (nombreUsuario != null || nombreUsuario !=0) {
 			sessionStorage.setItem('logueado', 'true');//cambia el estado del usuario a logueado
 			localStorage.setItem('nombreUsuario', nombreUsuario);//guarda en un local storage el nombre del usuario
 			window.location.replace("index.html");//redirije al index.html
 			return true;//hace que al final la información se envíe al servidor
-		}
+		}*/
 
 }
 function signOut() {
