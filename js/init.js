e76208f2223932319f44e4a9bf263a76b562f399
sabrinagayo <sabrinagayo6@gmail.com>
2020-11-20 19:55:56 -0300

    +prueba

--- js/init.js
+++ js/init.js
@@ -1,3 +1,4 @@
+  
 const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
 const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
 const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
